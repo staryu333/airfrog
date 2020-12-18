55b9ce3d8ade32afa636c3da18baec7cfb06f2e8
staryu333 <71486628+staryu333@users.noreply.github.com>
2020-12-18 15:08:54 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://staryu333.github.io/airfrog/index.html/model.json';
 
 
 function preload() {
